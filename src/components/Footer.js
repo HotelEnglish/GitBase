8637b98bcd10b1a0b24601a72e3225dd1defa1c5
Miro <45430033+HotelEnglish@users.noreply.github.com>
2024-09-26 13:56:12 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,13 @@ export function Footer() {
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
           <div>
-            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">About</h3>
+            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">关于</h3>
             <p className="mt-4 text-base text-gray-500">
-              GitBase is an open-source dynamic website solution without a traditional database, built with Next.js and powered by GitHub.
+              龚老师的折腾笔记，记录着龚老师在虚拟的网络世界摸爬滚打和荣辱辛酸。
             </p>
           </div>
           <div>
-            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">Quick Links</h3>
+            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">页面导航</h3>
             <ul className="mt-4 space-y-4">
               <li>
                 <Link href="/" className="text-base text-gray-500 hover:text-gray-900">
@@ -33,7 +33,7 @@ export function Footer() {
             </ul>
           </div>
           <div>
-            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">Connect</h3>
+            <h3 className="text-sm font-semibold text-gray-600 tracking-wider uppercase">友情链接</h3>
             <ul className="mt-4 space-y-4">
               <li>
                 <a href="https://gitbase.app/" target="_blank" className="text-base text-gray-500 hover:text-gray-900">
@@ -55,7 +55,7 @@ export function Footer() {
         </div>
         <div className="mt-8 border-t border-gray-200 pt-8">
           <p className="text-base text-gray-400 text-center">
-            &copy; {new Date().getFullYear()} GitBase. All rights reserved.
+            &copy; {new Date().getFullYear()} 自豪地采用 <span style="color: blue"> GitBase </span> 技术 by 龚老师 
           </p>
         </div>
       </div>
